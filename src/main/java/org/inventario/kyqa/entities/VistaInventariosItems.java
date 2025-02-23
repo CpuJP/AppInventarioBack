380228b001ff108f7878129f827176eaa19bb4a9
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2025-02-23 17:08:41 -0500

    UPDATE config entitiy VistaInventarioItem

--- src/main/java/org/inventario/kyqa/entities/VistaInventariosItems.java
+++ src/main/java/org/inventario/kyqa/entities/VistaInventariosItems.java
@@ -1,18 +1,17 @@
 package org.inventario.kyqa.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Immutable;
 
 import java.time.Instant;
 
 @Entity
+@Immutable // Marca la entidad como inmutable (solo lectura)
 @Table(name = "vista_inventarios_items")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class VistaInventariosItems {
 
     @Id
